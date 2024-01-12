ce46b2723bb75e8c82dea8c7ce85b3e35bdd82e2
Pranali <154823375+pranalibagul26@users.noreply.github.com>
2024-01-12 16:20:27 +0530

    Add files via upload

---
+++ int_array.java
@@ -0,0 +1,22 @@
+// create an array of int type and store 10 elements in array
+
+class int_array
+{
+public static void main(String[]args)
+{
+int arr[]=new int[10];
+arr[0]=100;
+arr[1]=200;
+arr[2]=300;
+arr[3]=400;
+arr[4]=500;
+arr[5]=600;
+arr[6]=700;
+arr[7]=80;
+arr[8]=90;
+arr[9]=100;
+
+System.out.println(arr[3]);
+
+}
+}
