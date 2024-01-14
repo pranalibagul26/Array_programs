9536457ab5ef1bc8d026a885e29eb48e6e6354dd
Pranali <154823375+pranalibagul26@users.noreply.github.com>
2024-01-14 22:59:16 +0530

    String type of array

---
+++ string_array.java
@@ -0,0 +1,24 @@
+// create an array of string type and store 10 elements in array
+
+
+class string_array
+{
+public static void main(String[]args)
+{
+
+String arr[]=new String[10];
+arr[0]= "Pranali";
+arr[1]= "ritika";
+arr[2]= "siddhi";
+arr[3]= "sakshi";
+arr[4]= "neha";
+arr[5]= "fatima";
+arr[6]= "mamini";
+arr[7]= "Darshana";
+arr[8]= "pooja";
+arr[9]= "Divya";
+
+System.out.println(arr[9]);
+
+}
+}
