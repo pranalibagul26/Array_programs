ce7bf53f82b16986980a2265fd46c4cf32256983
Pranali <154823375+pranalibagul26@users.noreply.github.com>
2024-01-16 22:56:06 +0530

    Add files via upload

---
+++ Double_array.java
@@ -0,0 +1,22 @@
+
+class Double_array
+{
+public static void main(String[]args)
+{
+
+double arr[]=new double[10];
+arr[0]=0.1;
+arr[1]=0.02;
+arr[2]=0.03;
+arr[3]=0.04;
+arr[4]=0.05;
+arr[5]=0.06;
+arr[6]=0.07;
+arr[7]=0.08;
+arr[8]=0.09;
+arr[9]=0.10;
+
+System.out.println(arr[7]);
+
+}
+}
---
+++ char_array.java
@@ -0,0 +1,25 @@
+// create an array of char type and store 10 elements in array
+
+
+class char_array
+{
+public static void main(String[]args)
+{
+
+char arr[]=new char[10];
+arr[0]='a';
+arr[1]='b';
+arr[2]='c';
+arr[3]='d';
+arr[4]='e';
+arr[5]='f';
+arr[6]='g';
+arr[7]='h';
+arr[8]='i';
+arr[9]='j';
+
+
+System.out.println(arr[6]);
+
+}
+}
